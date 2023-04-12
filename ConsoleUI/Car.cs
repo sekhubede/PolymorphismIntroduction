ef37d6461f066ad3014d90835ab0d8841acc5158
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-12 16:58:02 +0200

    Created a base class Car with some logic

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    public class Car
+    {
+        public int HP { get; set; }
+        public string Color { get; set; }
+
+        public Car(int hp, string color)
+        {
+            this.HP = hp;
+            this.Color = color;
+        }
+
+        public virtual void ShowDetails()
+        {
+            WriteLine($"HP: {this.HP} Color: {this.Color}");
+        }
+
+        public virtual void Repair()
+        {
+            WriteLine("Car was repaired.");
+        }
+    }
+}
