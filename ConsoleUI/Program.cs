1a19eb4e8f28366114104090c5e57b4535b3b2aa
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-12 16:32:51 +0200

    Added challenge instructions to help understand polymorphism

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using static System.Console;
 
 namespace ConsoleUI
 {
     internal class Program
     {
+        // Challenge
+        // Create a base class Car with two proerties HP and Color
+        // Create a Constructor setting those two properties
+        // Create a Method called ShowDetails() which shows the HP and Color of the car on the console
+        // Create a Repair Method which writes "Car was repaired!" onto the console
+        // Create two deriving classes, BMW and Audi, which have their own constructor and have an aditional property
+        // called Model. Also a private member called brand. Brand should be different in each of the two classes.
+        // Create the two methods ShowDetails() and Repair in them as well. Adjust those methods accordingly.
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
