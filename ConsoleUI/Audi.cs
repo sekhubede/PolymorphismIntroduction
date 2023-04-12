dda578c81deccc84aeeec80e008fdd46cfd65f7f
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-12 16:59:12 +0200

    Created an Audi class that that extends the Car class

---
+++ ConsoleUI/Audi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    public class Audi : Car
+    {
+        private string Brand = "Audi";
+
+        public string Model { get; set; }
+
+        public Audi(int hp, string color, string model, string brand) : base(hp, color)
+        {
+            this.Model = model;
+        }
+
+        public override void ShowDetails()
+        {
+            WriteLine($"Brnd: {Brand} HP: {this.HP} Color: {this.Color}");
+        }
+
+        public override void Repair()
+        {
+            WriteLine($"The {Brand} was repaired.");
+        }
+
+    }
+}
