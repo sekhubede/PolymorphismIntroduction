211b14dc910328029528a07ae2044570b2e96edf
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-12 16:59:00 +0200

    Created a BMW class that that extends the Car class

---
+++ ConsoleUI/BMW.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    public class BMW : Car
+    {
+        private string Brand = "BMW";
+
+        public string Model { get; set; }
+
+        public BMW(int hp, string color, string model) : base (hp, color)
+        {
+            this.Model = model;
+        }
+
+        public override void ShowDetails()
+        {
+            WriteLine($"Brnd: {Brand} HP: {this.HP} Color: {this.Color}");
+        }
+
+        public override void Repair()
+        {
+            WriteLine($"The {Brand} was repaired.");
+        }
+
+    }
+}
