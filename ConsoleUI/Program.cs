75bbd5e4eb1f080afbee5d26426e80a67209311d
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-25 14:30:53 +0200

    Updated the logic of the Program class.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -37,7 +37,7 @@ namespace ConsoleUI
                 car.Repair();
             }
 
-            ReadKey(true);
+            WriteLine();
 
             Car bmwZ3 = new BMW(200, "black", "Z3");
             Car audiA3 = new Audi(100, "green", "A3");
@@ -45,7 +45,7 @@ namespace ConsoleUI
             bmwZ3.ShowDetails();
             audiA3.ShowDetails();
 
-            ReadKey(true);
+            WriteLine();
 
             BMW bmwM5 = new BMW(330, "white", "M5");
 
@@ -54,6 +54,11 @@ namespace ConsoleUI
             // Casting, use the base class method on a deriving class method that has it's own method.
             Car carB = (Car)bmwM5;
             carB.ShowDetails();
+
+            WriteLine();
+
+            //M3 myM3 = new M3(260, "red", "M3 Super Turbo");
+            //myM3.Repair();
 
             ReadKey(true);
         }
