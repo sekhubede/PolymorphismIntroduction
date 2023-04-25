c6dcae9dedc1fa17632c0294e346b9abf097f7d6
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-25 15:29:44 +0200

    Added a CarIDInfo class to demo 'Has a relationship'

---
+++ ConsoleUI/CarIDInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    // Has a
+
+    public class CarIDInfo
+    {
+        public int IDNumber { get; set; } = 0;
+        public string Owner { get; set; } = "No owner";
+    }
+}
