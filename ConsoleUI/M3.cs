99ca1a76b24653d219bc777cd3b6000ad254e6d5
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-04-25 14:31:47 +0200

    Commented out the class logic as the base class is sealed.
    Just to show the use case of the sealed keyword.

--- ConsoleUI/M3.cs
+++ ConsoleUI/M3.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
-    internal class M3
+    // cannot inherit from a sealed class.
+    /*
+    internal class M3 : BMW
     {
+        public M3(int hp, string color, string model) : base(hp, color, model)
+        {
+            this.Model = model;
+        }
+
+        // unable to override this method because it is sealed.
+        //public override void Repair()
+        //{
+        //    base.Repair();
+        //}
     }
+    */
 }
